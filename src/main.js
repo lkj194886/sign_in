1abf079f8beb84d26398cd5c7101f863437cd76c
wgg <1748495608@qq.com>
2020-10-11 19:45:39 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -4,11 +4,8 @@ import 'vant/lib/index.css';
 import router from './router'
 import store from "./store/index"
 
-import plus from 'vue-html5plus'
-// Vue.use(plus);
 Vue.config.productionTip = false
-// Vue.prototype.$plus=plus
-//引入axios
+
 import axios from "axios";
 axios.defaults.headers.post["Content-type"] = "application/json";
 axios.defaults.baseURL = "http://192.168.0.102:8090";
