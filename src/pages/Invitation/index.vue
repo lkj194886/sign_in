6b6ea4f50d7cbce6dc4f2fc0d2e73a47ef6361f3
llxzuishuai <3475537380@qq.com>
2020-10-11 17:07:54 +0800

    update

--- src/pages/Invitation/index.vue
+++ src/pages/Invitation/index.vue
@@ -50,32 +50,55 @@
     //     this.toSave(hreLocal,"xiaolin")
     //   });
     // },
-      toSave(url,name) {
-        let image = new Image()
-        image.setAttribute('crossOrigin', 'anonymous')
-        image.src = url
-        image.onload = () => {
-          let canvas = document.createElement('canvas')
-          canvas.width = image.width
-          canvas.height = image.height
-          let ctx = canvas.getContext('2d')
-          ctx.drawImage(image, 0, 0, image.width, image.height)
-          canvas.toBlob((blob) => {
-            let url = URL.createObjectURL(blob)
-            this.download(url,name)
-            // 用完释放URL对象
-            URL.revokeObjectURL(url)
-          })
-        }
-      },
-      download(href, name) {
-       let eleLink = document.createElement('a')
-       eleLink.download = name
-       eleLink.href = href
-       eleLink.click()
-       eleLink.remove()
-     }
+    //   toSave(url,name) {
+    //     let image = new Image()
+    //     image.setAttribute('crossOrigin', 'anonymous')
+    //     image.src = url
+    //     image.onload = () => {
+    //       let canvas = document.createElement('canvas')
+    //       canvas.width = image.width
+    //       canvas.height = image.height
+    //       let ctx = canvas.getContext('2d')
+    //       ctx.drawImage(image, 0, 0, image.width, image.height)
+    //       canvas.toBlob((blob) => {
+    //         let url = URL.createObjectURL(blob)
+    //         this.download(url,name)
+    //         // 用完释放URL对象
+    //         URL.revokeObjectURL(url)
+    //       })
+    //     }
+    //   },
+    //   download(href, name) {
+    //    let eleLink = document.createElement('a')
+    //    eleLink.download = name
+    //    eleLink.href = href
+    //    eleLink.click()
+    //    eleLink.remove()
+    //  }
       
+toSave(imgsrc, name) {//下载图片地址和图片名
+  var image = new Image();
+  // 解决跨域 Canvas 污染问题
+  image.setAttribute("crossOrigin", "anonymous");
+  image.onload = function() {
+    var canvas = document.createElement("canvas");
+    canvas.width = image.width;
+    canvas.height = image.height;
+    var context = canvas.getContext("2d");
+    context.drawImage(image, 0, 0, image.width, image.height);
+    var url = canvas.toDataURL("image/png"); //得到图片的base64编码数据
+ 
+    var a = document.createElement("a"); // 生成一个a元素
+    var event = new MouseEvent("click"); // 创建一个单击事件
+    a.download = name || "photo"; // 设置图片名称
+    a.href = url; // 将生成的URL设置为a.href属性
+    a.dispatchEvent(event); // 触发a的单击事件
+  };
+  image.src = imgsrc;
+},
+downs(){
+  this.toSave(this.pic.url, 'pic')
+}
     }
   };
 </script>
